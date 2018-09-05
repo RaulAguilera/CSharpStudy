44c4a75d15f724347d8497b690cb553f080c1adc
Raul Aguilera <raguilera@mty.ipointsystems.com>
2018-09-05 10:46:13 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopped = false;
+
+            Thread t = new Thread(new ThreadStart(() =>
+            {
+
+                while (!stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(1000);
+                }
+
+
+            }));
+
+            t.Start();
+            Console.WriteLine("Press key to exit");
+            Console.ReadKey();
+
+            stopped = true;
+            t.Join();
+
+        }
+
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
+
